95fe16f00ac22d987c3df70dfc5403ec20cc2aec
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-11-20 08:28:14 +0000

    Lab-3-Zadanie-7

--- lab3.cpp
+++ lab3.cpp
@@ -4,9 +4,13 @@
 class Figura
 {
 public:
+    Figura() : pole{0} { std::cout << "Konstruktor domyslny - Figura" << std::endl; }
+    Figura(double p) : pole{p} { std::cout << "Konstruktor parametryczny - Figura" << std::endl; }
+    ~Figura() { std::cout << "Destruktor - Figura" << std::endl; }
+
     double getPole() const;
 
-protected:
+private:
     double pole;
 };
 
@@ -18,15 +22,23 @@ double Figura::getPole() const
 class Kwadrat : public Figura
 {
 public:
-    Kwadrat() { pole = 0.; }
-    Kwadrat(double a) { pole = a * a; }
+    Kwadrat() : Figura{0.} { std::cout << "Konstruktor domyslny - Kwadrat" << std::endl; }
+    Kwadrat(double a) : Figura{a * a}
+    {
+        std::cout << "Konstruktor parametryczny - Kwadrat" << std::endl;
+    }
+    ~Kwadrat() { std::cout << "Destruktor - Kwadrat" << std::endl; }
 };
 
 class Kolo : public Figura
 {
 public:
-    Kolo() { pole = 0.; }
-    Kolo(double r) { pole = M_PI * r * r; }
+    Kolo() : Figura{0.} { std::cout << "Konstruktor domyslny - Kolo" << std::endl; }
+    Kolo(double r) : Figura{M_PI * r * r}
+    {
+        std::cout << "Konstruktor parametryczny - Kolo" << std::endl;
+    }
+    ~Kolo() { std::cout << "Destruktor - Kolo" << std::endl; }
 };
 
 int main()
