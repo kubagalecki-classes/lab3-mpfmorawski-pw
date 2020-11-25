6aae014f4b8dc7c57e4b43deb95516e62c71e53d
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-11-25 18:35:07 +0000

    Zal

--- zal/transportujFlota.hpp
+++ zal/transportujFlota.hpp
@@ -4,6 +4,16 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    unsigned int przetransportowany_towar = 0;
+    unsigned int zaglowiec_counter = 0;
+    while(przetransportowany_towar < towar)
+    {
+      Stocznia stocznia{};
+      Statek* statek = stocznia();
+      przetransportowany_towar += (statek->transportuj());
+      if ((dynamic_cast<Zaglowiec *>(statek)) != nullptr)
+        zaglowiec_counter++;
+      delete statek;
+    }
+    return zaglowiec_counter;
 }
